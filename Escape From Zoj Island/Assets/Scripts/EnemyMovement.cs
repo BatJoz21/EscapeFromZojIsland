faf522feea908d51139ad664227d51b27e33725c
Jozka Siregar <jozkasiregar@gmail.com>
2024-04-01 20:20:41 +0700

    enemies and hazards

---
+++ Escape From Zoj Island/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Rigidbody2D rbEnemy;
+
+    [SerializeField] private float enemyMoveSpeed = 5.0f;
+
+    void Start()
+    {
+        rbEnemy = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rbEnemy.velocity = new Vector2(-enemyMoveSpeed, 0f);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("test");
+        enemyMoveSpeed = -enemyMoveSpeed;
+        FlipEnemyFacing();
+    }
+
+    private void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2(Mathf.Sign(rbEnemy.velocity.x), 1f);
+    }
+}
--- Escape From Zoj Island/Assets/Scripts/PlayerMovement.cs
+++ Escape From Zoj Island/Assets/Scripts/PlayerMovement.cs
@@ -9,31 +9,98 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 moveInput;
     private Rigidbody2D rb2d;
     private Animator anim;
+    private CapsuleCollider2D capsuleCollider;
+    private BoxCollider2D boxCollider;
+    private int platformLayerMask;
+    private int ladderLayerMask;
+    private float playerGravity;
+    private bool isAlive = true;
 
     [SerializeField] private float runSpeed = 5.0f;
+    [SerializeField] private float jumpSpeed = 5.0f;
+    [SerializeField] private float climbSpeed = 5.0f;
+    [SerializeField] private Vector2 deathKick = new Vector2(0f, 10f);
 
-    void Start()
+    void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Start()
+    {
+        playerGravity = rb2d.gravityScale;
+        ladderLayerMask = LayerMask.GetMask("Ladder");
     }
 
     void Update()
     {
-        Run();
-        FlipSprite();
+        if (isAlive)
+        {
+            Run();
+            FlipSprite();
+            ClimbingLadder();
+        }
+        else
+        {
+            Death();
+        }
     }
 
     private void OnMove(InputValue value)
     {
+        if (!isAlive) { return; }
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
     }
 
     private void Run()
     {
+        if (!isAlive) { return; }
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb2d.velocity.y);
         rb2d.velocity = playerVelocity;
+    }
+
+    private void OnJump(InputValue value)
+    {
+        if (!isAlive) { return; }
+        platformLayerMask = LayerMask.GetMask("Ground");
+        if (value.isPressed)
+        {
+            if (boxCollider.IsTouchingLayers(platformLayerMask))
+            {
+                rb2d.velocity += new Vector2(rb2d.velocity.x, jumpSpeed);
+                anim.SetBool("isRunning", false);
+                anim.SetBool("isJumping", true);
+            }
+        }
+    }
+
+    private void ClimbingLadder()
+    {
+        if (!isAlive) { return; }
+        bool isClimbing = Mathf.Abs(rb2d.velocity.y) > Mathf.Epsilon;
+
+        if (capsuleCollider.IsTouchingLayers(ladderLayerMask))
+        {
+            Vector2 playerUp = new Vector2(rb2d.velocity.x, moveInput.y * climbSpeed);
+            rb2d.velocity = playerUp;
+            rb2d.gravityScale = 0;
+            if (isClimbing)
+            {
+                anim.SetBool("isClimbing", true);
+            }
+            else
+            {
+                anim.SetBool("isClimbing", false);
+            }
+        }
+        else
+        {
+            anim.SetBool("isClimbing", false);
+            rb2d.gravityScale = playerGravity;
+        }
     }
 
     private void FlipSprite()
@@ -44,10 +111,29 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector2(Mathf.Sign(rb2d.velocity.x), 1f);
             anim.SetBool("isRunning", true);
+            anim.SetBool("isJumping", false);
         }
         else
         {
             anim.SetBool("isRunning", false);
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Enemies" || collision.collider.tag == "Hazards")
+        {
+            if (isAlive)
+            {
+                isAlive = false;
+                rb2d.velocity = deathKick;
+            }
+            isAlive = false;
+        }
+    }
+
+    private void Death()
+    {
+        anim.SetTrigger("Dead");
     }
 }
